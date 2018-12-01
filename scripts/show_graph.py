3faba92bf046df3ecfd98765870624cdd4a11146
Archer Zhang <archer.xn@gmail.com>
2018-12-01 01:55:21 -0500

    visualizing script and write to wave file (#2)
    * add script for visualizing wave file
    
    * implement streamer module

---
+++ scripts/show_graph.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+import sys
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--file", required=True, help="wave file to visualize")
+args = vars(ap.parse_args())
+
+spf = wave.open(args["file"],'r')
+
+#Extract Raw Audio from Wav File
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'Int16')
+
+
+#If Stereo
+if spf.getnchannels() == 2:
+    print 'Just mono files'
+    sys.exit(0)
+
+plt.figure(1)
+plt.title('Signal Wave...')
+plt.plot(signal)
+plt.show()
