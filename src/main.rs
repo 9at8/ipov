14747c93f9dfc0a1ee28f69a9f0906c20e74522c
Winnie <winnierjren@gmail.com>
2018-12-01 00:11:13 -0500

    recorder function added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate ears;
+
+use std::thread::sleep;
+use std::time::Duration;
+use ears::AudioController;
+
 fn main() {
-    println!("Hello, world!");
+    // initialize the RecordContext
+    let ctxt = ears::init_in().expect("Initialization error!");
+
+    // Create a new Recorder using the RecordContext
+    let mut recorder = ears::Recorder::new(ctxt);
+    println!("Recording for 3 seconds");
+    recorder.start();
+    sleep(Duration::from_millis(3000));
+    recorder.stop();
+    match recorder.save_to_file("hello") {
+        true => println!("Save okay!"),
+        false => println!("Cannot save ...")
+    }
+
+    println!("Playing hello.wav");
+    let mut sound = ears::Sound::new("hello.wav").unwrap();
+    sound.play();
+    while sound.is_playing() {}
 }
